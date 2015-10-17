0b430ed6975d475829e29f2cda0dbd9d1c64e051
Eileen Conner <eileenconner@gmail.com>
2015-10-17 10:48:36 -0700

    sql section completed to 2.3

--- skills-sql/skills.sql
+++ skills-sql/skills.sql
@@ -28,19 +28,24 @@ SELECT * FROM Brands WHERE founded < 1903 GROUP BY name ORDER BY founded ASC;
 SELECT COUNT(*) FROM Models WHERE brand_name = 'Ford';
 
 -- 7. Select the name of any and all car brands that are not discontinued.
-SELECT name FROM Brands WHERE discontinued NOT NULL;
---  This is the opposite of what we want! FIX ME
-
+SELECT name FROM Brands WHERE discontinued IS NULL;
 
 -- 8. Select rows 15-25 of the DB in alphabetical order by model name.
+SELECT * FROM Models WHERE id BETWEEN 15 AND 25 ORDER BY brand_name;
+-- I believe we can also do this with offset/limit by instead of between, 
+-- but Ask Dr Google returned this answer, which seems even better.
+-- Also, the html instructions specified "Models" instead of the entire db.
 
 -- 9. Select the brand, name, and year the model's brand was 
 --    founded for all of the models from 1960. Include row(s)
 --    for model(s) even if its brand is not in the Brands table.
 --    (The year the brand was founded should be NULL if 
 --    the brand is not in the Brands table.)
-
-
+SELECT brand_name, Models.name, founded 
+FROM Models LEFT JOIN Brands 
+ON (Brands.name=Models.brand_name) 
+WHERE Models.year = 1960;
+-- This solution leaves the null field blank. Not sure how to get it to print.
 
 -- Part 2: Change the following queries according to the specifications. 
 -- Include the answers to the follow up questions in a comment below your
@@ -57,6 +62,11 @@ SELECT name FROM Brands WHERE discontinued NOT NULL;
     --     ON b.name = m.brand_name
     -- WHERE b.discontinued IS NULL;
 
+SELECT b.name, b.founded, m.name 
+FROM Brands as b LEFT JOIN Models as m 
+ON b.name = m.brand_name 
+WHERE b.discontinued IS NULL;
+
 -- 2. Modify this left join so it only selects models that have brands in the Brands table.
 -- before: 
     -- SELECT m.name,
@@ -66,8 +76,16 @@ SELECT name FROM Brands WHERE discontinued NOT NULL;
     --   LEFT JOIN Brands AS b
     --     ON b.name = m.brand_name;
 
+SELECT m.name, m.brand_name, b.founded
+FROM Models AS m JOIN Brands AS b 
+ON b.name = m.brand_name;
+
 -- followup question: In your own words, describe the difference between 
 -- left joins and inner joins.
+
+-- Left joins contain all columns from the first table listed in the join, 
+-- and only those that match the specific query from the second table.
+-- Inner joins only contain the columns that overlap both tables.
 
 -- 3. Modify the query so that it only selects brands that don't have any models in the models table. 
 -- (Hint: it should only show Tesla's row.)
@@ -78,6 +96,10 @@ SELECT name FROM Brands WHERE discontinued NOT NULL;
     --   LEFT JOIN Models
     --     ON brands.name = Models.brand_name
     -- WHERE Models.year > 1940;
+SELECT Brands.name, founded 
+FROM Brands LEFT JOIN Models 
+ON brands.name = Models.brand_name 
+WHERE Models.name IS NULL;
 
 -- 4. Modify the query to add another column to the results to show 
 -- the number of years from the year of the model until the brand becomes discontinued
